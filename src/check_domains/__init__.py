ce67900ebf77c8560824dba60554ff5e07d44784
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2025-01-14 22:46:41 +0500

    feat: Add argparse for domain input

--- src/check_domains/__init__.py
+++ src/check_domains/__init__.py
@@ -1,2 +1,14 @@
+import argparse
+
+
 def main() -> None:
-    print("Hello from check-domains!")
+    parser = argparse.ArgumentParser(description="Check domains.")
+    parser.add_argument(
+        "domains",
+        metavar="DOMAIN",
+        type=str,
+        nargs="+",
+        help="Domain to check.",
+    )
+    args = parser.parse_args()
+    print(f"Hello from check-domains! You passed these domains: {args.domains}")
